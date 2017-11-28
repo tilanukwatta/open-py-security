ec0fd68beec99abd263da58121ea97cb554cd3f4
tilan <tilan@vps135653.vps.ovh.ca>
2017-11-28 17:12:47 -0500

    added initial main run file

---
+++ runme.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from datetime import timedelta, datetime
+import time
+
+
+if __name__ == '__main__':
+    
+    print(datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0))
+
+    #import ipdb; ipdb.set_trace() # debugging code
+
