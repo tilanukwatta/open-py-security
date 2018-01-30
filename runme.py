e55a876eb6e378c5cce3b1aafd3d3fab85a07370
Tilan Ukwatta <tilan.ukwatta@gmail.com>
2018-01-30 10:32:24 -0500

    added initial code to capture images

--- runme.py
+++ runme.py
@@ -2,11 +2,20 @@
 
 from datetime import timedelta, datetime
 import time
+import picamera
 
 
 if __name__ == '__main__':
     
     print(datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0))
+    
+    camera = picamera.PiCamera()
 
-    import ipdb; ipdb.set_trace() # debugging code
+    record = True
+    while record:
+      image_name = "image.jpg"
+      camera.capture()
+      time.sleep(60)  
+
+    #import ipdb; ipdb.set_trace() # debugging code
 
